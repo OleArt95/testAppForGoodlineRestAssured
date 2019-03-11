fc2488ba48ad628eeba681c65e35c14b7cf6efc2
Oleg Art <Wickodin@gmail.com>
2019-03-12 03:47:12 +0700

    Изменения:
    1. Добавил метод configuration для базовой настройки основных
    параметров (BaseUri, BasePath, ContentType), необходимых для
    RestAssured + токен доступа и версию API
    2. Завел константы: HTTP_STATUS_OK, URL_API_VK_COM, BASE_PATH,
    API_VERSION
    3. Убрал повторяющиеся куски кода в тестовых методах.

--- src/test/java/VkTestClass.java
+++ src/test/java/VkTestClass.java
@@ -1,44 +1,54 @@
 import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 
 public class VkTestClass {
 
+    private static final int HTTP_STATUS_OK = 200;
+    private static final String URL_API_VK_COM = "https://api.vk.com";
+    private static final String BASE_PATH = "/method/";
+    private static final String API_VERSION = "5.52";
+
+    @Before
+    public void configuration() {
+    RequestSpecification basicSpecification = new RequestSpecBuilder()
+            .setBaseUri(URL_API_VK_COM)
+            .setBasePath(BASE_PATH)
+            .setContentType(ContentType.JSON)
+            .addParam("access_token","b4f2daabf13912c20411dd88eb3ca5fd6b23d8b1fca2c8f003efcc6dcb0d89f2968820eab8a4f099e8746")
+            .addParam("v", API_VERSION)
+            .build();
+
+    RestAssured.requestSpecification = basicSpecification;
+    }
+
     @Test
     public void getUserInfoWithMethodUsersGet() {
         RequestSpecification requestSpecification = RestAssured.given();
 
-        requestSpecification.header("Content-Type", String.valueOf(ContentType.JSON));
-
         requestSpecification.param("user_ids", "ole_art");
 
-        requestSpecification.param("access_token", "b4f2daabf13912c20411dd88eb3ca5fd6b23d8b1fca2c8f003efcc6dcb0d89f2968820eab8a4f099e8746");
-        requestSpecification.param("v", "5.52");
-
-        Response response = requestSpecification.get("https://api.vk.com/method/users.get");
+        Response response = requestSpecification.get("users.get");
 
         System.out.println(response.getBody().asString());
 
-        Assert.assertEquals(200, response.statusCode());
+        Assert.assertEquals(HTTP_STATUS_OK, response.statusCode());
     }
 
     @Test
     public void getUserInfoWithMethodUserGetWithFields() {
         RequestSpecification requestSpecification = RestAssured.given();
 
-        requestSpecification.header("Content-Type", String.valueOf(ContentType.JSON));
-
         requestSpecification.param("user_ids", "ole_art");
         requestSpecification.param("fields", "city, sex");
 
-        requestSpecification.param("access_token", "b4f2daabf13912c20411dd88eb3ca5fd6b23d8b1fca2c8f003efcc6dcb0d89f2968820eab8a4f099e8746");
-        requestSpecification.param("v", "5.52");
-
-        Response response = requestSpecification.get("https://api.vk.com/method/users.get");
+        Response response = requestSpecification.get("users.get");
 
         System.out.println(response.getBody().asString());
         System.out.println(response.path("response[0].city"));
@@ -47,7 +57,7 @@ public class VkTestClass {
         Assert.assertEquals(2, response.path("response[0].sex"));
         Assert.assertEquals("Олег", response.path("response[0].first_name"));
         Assert.assertEquals("Артюхов", response.path("response[0].last_name"));
-        Assert.assertEquals(200, response.statusCode());
+        Assert.assertEquals(HTTP_STATUS_OK, response.statusCode());
     }
 
     @Ignore
@@ -55,16 +65,11 @@ public class VkTestClass {
     public void getInfoWithMethodSearchGetHints() {
         RequestSpecification requestSpecification = RestAssured.given();
 
-        requestSpecification.header("Content-Type", String.valueOf(ContentType.JSON));
-
         requestSpecification.param("q", "Типичный Кемерово");
         requestSpecification.param("limit", "10");
         requestSpecification.param("search_global", 1);
 
-        requestSpecification.param("access_token", "b4f2daabf13912c20411dd88eb3ca5fd6b23d8b1fca2c8f003efcc6dcb0d89f2968820eab8a4f099e8746");
-        requestSpecification.param("v", "5.92");
-
-        Response response = requestSpecification.get("https://api.vk.com/method/search.getHints");
+        Response response = requestSpecification.get("search.getHints");
 
         System.out.println(response.getBody().asString());
     }
@@ -73,20 +78,15 @@ public class VkTestClass {
     public void getInfoWithMethodUsersSearch() {
         RequestSpecification requestSpecification = RestAssured.given();
 
-        requestSpecification.header("Content-Type", String.valueOf(ContentType.JSON));
-
         requestSpecification.param("q", "Алексей Леонов");
         requestSpecification.param("count", "4");
         requestSpecification.param("hometown", "Кемерово");
 
-        requestSpecification.param("access_token", "b4f2daabf13912c20411dd88eb3ca5fd6b23d8b1fca2c8f003efcc6dcb0d89f2968820eab8a4f099e8746");
-        requestSpecification.param("v", "5.92");
-
-        Response response = requestSpecification.get("https://api.vk.com/method/users.search");
+        Response response = requestSpecification.get("users.search");
 
         System.out.println(response.getBody().asString());
 
-        Assert.assertEquals(200, response.statusCode());
+        Assert.assertEquals(HTTP_STATUS_OK, response.statusCode());
         Assert.assertEquals("Леонов", response.path("response.items[2].last_name"));
     }
 
@@ -94,20 +94,15 @@ public class VkTestClass {
     public void getInfoWithMethodUsersGetFollowers() {
         RequestSpecification requestSpecification = RestAssured.given();
 
-        requestSpecification.header("Content-Type", String.valueOf(ContentType.JSON));
-
         requestSpecification.param("user_id", "228270452");
         requestSpecification.param("count", "2");
         requestSpecification.param("fields", "first_name, last_name");
 
-        requestSpecification.param("access_token", "b4f2daabf13912c20411dd88eb3ca5fd6b23d8b1fca2c8f003efcc6dcb0d89f2968820eab8a4f099e8746");
-        requestSpecification.param("v", "5.92");
-
-        Response response = requestSpecification.get("https://api.vk.com/method/users.getFollowers");
+        Response response = requestSpecification.get("users.getFollowers");
 
         System.out.println(response.getBody().asString());
 
-        Assert.assertEquals(200, response.statusCode());
+        Assert.assertEquals(HTTP_STATUS_OK, response.statusCode());
         Assert.assertEquals("Лазина", response.path("response.items[1].last_name"));
     }
 }
